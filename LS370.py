4d7a0672379cc2dfd91c711151c33720c7435cb6
Benjamin Schmidt <schmidtb@physics.mcgill.ca>
2013-02-11 14:08:47 -0500

    Updated LS370 driver to use pyVisa

--- LS370.py
+++ LS370.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python  
-from GPIB import *  
+import visa
+
 import string, os, sys, time  
       
 class LS370:  
-    def __init__(self, name):  
-        self.ls = Gpib(name)  
-        ls = self.ls 
+    def __init__(self, name, debug=False):  
+        self.debug = debug
+        if debug==True:
+            self.ls = None
+        else:
+            self.ls = visa.instrument(name)  
 
     def read_channel (self, chan):
-        self.write ('RDGR? ' + str(chan))      
-        data = self.read()
-        if data =="":
-            return 0
+        if self.debug == False:        
+            self.write ('RDGR? ' + str(chan))      
+            data = self.read()
+            if data =="":
+                return 0
+            else:
+                return float(data)
         else:
-            return float(data)
+            return 1337.0
+    
+    def set_heater_range(self, htr_range):
+        if self.debug == False:    
+            if htr_range >= 0 and htr_range < 9:
+                self.write('HTRRNG %d'%htr_range)
+    
+    def set_heater_output(self, percent):
+        if self.debug == False:          
+            if percent >= 0 and percent <= 100:
+                self.write('MOUT %.3f'%percent)
     
     def auto_scan (self):
-        self.write('SCAN 1,1')
+        if self.debug == False:        
+            self.write('SCAN 1,1')
 
     def scanner_to_channel(self, chan):
-        self.write('SCAN %d,0'%chan)
+        if self.debug == False:
+            self.write('SCAN %d,0'%chan)
 
         
     #lower level commands start here
       
-    def read(self,num=512):  
-        ''''' Use only for text data. '''  
-        ls = self.ls
-        a = ls.read(num)  
-        return a  
-  
-    def readb(self,num=512):  
-        ''''' Use to read binary data. This prevents early termination 
-            from a \0. '''  
-        srs = self.srs  
-        a = ls.readb(num)  
-        return a  
-      
+    def read(self):  
+        if self.debug == False:
+            return self.ls.read() 
+
     def write(self,stri):  
-        ls = self.ls  
-        ls.write(str(stri))  
+        if self.debug == False:
+            self.ls.write(str(stri))  
       
     def close(self):  
-        self.ls.close()  
+        if self.debug == False:
+            self.ls.close()  
 
 
     #if run as own program  
