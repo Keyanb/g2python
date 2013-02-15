df916ff9209386ba4243167f2687b6efb0172a0b
Benjamin Schmidt <schmidtb@physics.mcgill.ca>
2013-02-14 22:12:48 -0500

    Start adding channel params to recordsweep GUI

--- recordsweep/recordsweep.py
+++ recordsweep/recordsweep.py
@@ -7,8 +7,9 @@ Created on Sat Jun 16 13:18:32 2012
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
+from PyQt4 import QtCore, QtGui
 import ui_recordsweep
+import visa
 
 import LS370, HP4263B, MKS
 import time
@@ -32,7 +33,7 @@ class RecordSweepWindow(QMainWindow, ui_recordsweep.Ui_RecordSweepWindow):
     def __init__(self, parent=None):
         super(RecordSweepWindow, self).__init__()
         self.setupUi(self)
-        self.customizeUi()
+
         
         self.lock = QReadWriteLock()         
         self.datataker = DataTaker(self.lock, self)  
@@ -47,54 +48,106 @@ class RecordSweepWindow(QMainWindow, ui_recordsweep.Ui_RecordSweepWindow):
         self.fig.canvas.draw()              
         
          # objects to hold line data
-
-        #line1, = self.ax.plot(0, 1, '.r', )
-        #line2, = self.ax.plot(0, 0, '-g')
         line_set = []
+        fmt_str = ['b','g','r','k','c','m']
+        
         for i in range (self.MAX_CHANNELS):            
-            line1, = self.ax.plot(0, 1, '.r', )
+            line1, = self.ax.plot([], [], fmt_str[i])            
             line_set.append(line1)
         
         self.ax.lines = line_set
-
-        print self.ax.lines
-        #self.lines = [DataLine(line1, 0, 2, '.'), DataLine(line2, 0, 3, '-')]
+ 
+        self.customizeUi()
         
+        try:
+            print visa.get_instruments_list()
+        except visa.VisaIOError as e:
+            if e.error_code == -1073807343:
+                print "GPIB does not seem to be connected"
+
+            
         self.data_array = array([])       
         self.chan_X = 0
         
     def customizeUi(self):
-        
+
+        self.lineEdit_Name = []
+        self.comboBox_Type = []
+        types = ['Time', 'IPS120', 'SRS830']
+        self.comboBox_Instr = []
+        ports = ['GPIB::8']
         self.radioButton_X = [] 
+        self.checkBox_Y = []        
+
+
         
         for i in range (self.MAX_CHANNELS):                       
+            self.lineEdit_Name.append(QtGui.QLineEdit(self.groupBox_Name))
+            self.lineEdit_Name[i].setGeometry(QtCore.QRect(10, 20 * (i+1), 61, 16))
+            self.lineEdit_Name[i].setText(QtGui.QApplication.translate("RecordSweepWindow", "TIME", None, QtGui.QApplication.UnicodeUTF8))
+            self.lineEdit_Name[i].setObjectName(_fromUtf8("lineEdit_Name"))
+            
+            self.comboBox_Type.append(QtGui.QComboBox(self.groupBox_Type))
+            self.comboBox_Type[i].setGeometry(QtCore.QRect(10, 20 * (i+1), 71, 16))
+            self.comboBox_Type[i].setObjectName(_fromUtf8("comboBox"))
+            for j, item in enumerate(types):   
+               self.comboBox_Type[i].addItem(_fromUtf8(""))
+               self.comboBox_Type[i].setItemText(j, QtGui.QApplication.translate("RecordSweepWindow", item, None, QtGui.QApplication.UnicodeUTF8))
+
+            self.connect(self.comboBox_Type[i], SIGNAL("currentIndexChanged(int)"), self.ComboBoxTypeHandler)                  
+
+            self.comboBox_Instr.append(QtGui.QComboBox(self.groupBox_Instr))
+            self.comboBox_Instr[i].setGeometry(QtCore.QRect(10, 20 * (i+1), 71, 16))
+            self.comboBox_Instr[i].setObjectName(_fromUtf8("comboBox"))
+            for j, item in enumerate(ports):   
+               self.comboBox_Instr[i].addItem(_fromUtf8(""))
+               self.comboBox_Instr[i].setItemText(j, QtGui.QApplication.translate("RecordSweepWindow", item, None, QtGui.QApplication.UnicodeUTF8))
+
+            
+
             self.radioButton_X.append(QRadioButton(self.groupBox_X))
             self.radioButton_X[i].setGeometry(QRect(10, 20*(i+1), 16, 16))
             self.radioButton_X[i].setText(_fromUtf8(""))
             self.radioButton_X[i].setObjectName(_fromUtf8("radioButton_" + str(i)))
-            self.connect(self.radioButton_X[i], SIGNAL("toggled(bool)"), self.XRadioButtonHandler)       
-
-        
-        self.checkBox_Y = []
-        
-        for i in range (self.MAX_CHANNELS):          
+            self.connect(self.radioButton_X[i], SIGNAL("toggled(bool)"), self.XRadioButtonHandler)                          
+      
             self.checkBox_Y.append(QCheckBox(self.groupBox_Y))
             self.checkBox_Y[i].setGeometry(QRect(10, 20 * (i+1), 16, 16))
             self.checkBox_Y[i].setText(_fromUtf8(""))
             self.checkBox_Y[i].setObjectName(_fromUtf8("checkBox_" +str(i)))  
             self.connect(self.checkBox_Y[i], SIGNAL("stateChanged(int)"), self.YCheckBoxHandler)       
 
+
+
+            
+        self.radioButton_X[0].setChecked(True)  
+        self.checkBox_Y[2].setChecked(True)
+        self.checkBox_Y[3].setChecked(True)
+        
     def XRadioButtonHandler(self):
         for num, box in enumerate(self.radioButton_X):
             if box.isChecked():
                 self.chan_X = num
-        self.updatePlot() 
+        #self.updatePlot() 
         
     def YCheckBoxHandler(self):        
         for box in self.checkBox_Y:
             print box.isChecked()
-        self.updatePlot()      
- 
+        #self.updatePlot()      
+    
+    def ComboBoxTypeHandler(self):
+        for typeBox, instrBox in zip (self.comboBox_Type, self.comboBox_Instr):     
+            if typeBox == self.sender():
+                for i in range(instrBox.count() + 1):
+                        instrBox.removeItem(0)
+                        
+                text = typeBox.currentText()
+                if text == "Time":
+                    pass
+                else:
+                    instrBox.addItem("GPIB::8")
+                    instrBox.addItem("GPIB::9")
+             
     def updateData(self, data_set):        
         if self.data_array.size == 0:
             self.data_array = data_set
@@ -122,11 +175,36 @@ class RecordSweepWindow(QMainWindow, ui_recordsweep.Ui_RecordSweepWindow):
     @pyqtSignature("")
     def on_startStopButton_clicked(self):
 
-        if self.datataker.isStopped():      
-            self.datataker.initialize() 
+        if self.datataker.isStopped():    
+            name_list = []
+            for lineEdit in self.lineEdit_Name:
+                lineEdit.setReadOnly(True)
+                name_list.append(str(lineEdit.text()))
+                
+            type_list = []
+            for comboBox in self.comboBox_Type:
+                idx = comboBox.currentIndex()
+                text = comboBox.currentText() 
+                type_list.append(str(text))
+                print text
+                for i in range(comboBox.count() + 1):
+                    comboBox.removeItem(0)
+                comboBox.addItem(text)
+                
+            dev_list = []
+            for comboBox in self.comboBox_Instr:
+                idx = comboBox.currentIndex()
+                text = comboBox.currentText() 
+                if not (text in dev_list):
+                    dev_list.append(str(text))
+                    print text
+                
+                for i in range(comboBox.count() + 1):
+                    comboBox.removeItem(0)
+                comboBox.addItem(text)
             
- 
-           
+            
+            self.datataker.initialize(name_list, type_list, dev_list) 
             self.datataker.start()
             
             self.startStopButton.setText("Stop")
@@ -157,13 +235,15 @@ class DataTaker(QThread):
         self.completed = False
         self.DEBUG = readconfigfile.get_debug_setting()
 
-    def initialize(self, ):     
+    def initialize(self, name_list, type_list, device_list = []):     
         print self.DEBUG
         self.stopped = True
         self.completed = False     
         self.t_start = time.time()
         #instrumentation setup - store instrument objects in a dictionary by address
         self.instruments = {}
+                
+        
         self.instruments['GPIB::8'] = SRS830.SRS830('GPIB::8', debug=self.DEBUG)
         
         # tuple: lockin #, channel, subplot for display
--- recordsweep/ui_recordsweep.py
+++ recordsweep/ui_recordsweep.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'recordsweep.ui'
 #
-# Created: Thu Feb 14 12:13:12 2013
+# Created: Thu Feb 14 21:23:51 2013
 #      by: PyQt4 UI code generator 4.8.6
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,35 +17,40 @@ except AttributeError:
 class Ui_RecordSweepWindow(object):
     def setupUi(self, RecordSweepWindow):
         RecordSweepWindow.setObjectName(_fromUtf8("RecordSweepWindow"))
-        RecordSweepWindow.resize(966, 600)
+        RecordSweepWindow.resize(1035, 600)
         RecordSweepWindow.setWindowTitle(QtGui.QApplication.translate("RecordSweepWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
         self.centralwidget = QtGui.QWidget(RecordSweepWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.mplwidget = MatplotlibWidget(self.centralwidget)
-        self.mplwidget.setGeometry(QtCore.QRect(0, 0, 691, 571))
+        self.mplwidget.setGeometry(QtCore.QRect(0, 0, 641, 571))
         self.mplwidget.setObjectName(_fromUtf8("mplwidget"))
         self.startStopButton = QtGui.QPushButton(self.centralwidget)
         self.startStopButton.setGeometry(QtCore.QRect(700, 10, 75, 23))
         self.startStopButton.setText(QtGui.QApplication.translate("RecordSweepWindow", "Start", None, QtGui.QApplication.UnicodeUTF8))
         self.startStopButton.setObjectName(_fromUtf8("startStopButton"))
-        
         self.groupBox_Y = QtGui.QGroupBox(self.centralwidget)
-        self.groupBox_Y.setGeometry(QtCore.QRect(730, 360, 31, 191))
+        self.groupBox_Y.setGeometry(QtCore.QRect(940, 80, 31, 191))
         self.groupBox_Y.setTitle(QtGui.QApplication.translate("RecordSweepWindow", "Y", None, QtGui.QApplication.UnicodeUTF8))
         self.groupBox_Y.setObjectName(_fromUtf8("groupBox_Y"))
-      
-        
         self.groupBox_X = QtGui.QGroupBox(self.centralwidget)
-        self.groupBox_X.setGeometry(QtCore.QRect(700, 360, 31, 191))
+        self.groupBox_X.setGeometry(QtCore.QRect(910, 80, 31, 191))
         self.groupBox_X.setTitle(QtGui.QApplication.translate("RecordSweepWindow", "X", None, QtGui.QApplication.UnicodeUTF8))
         self.groupBox_X.setObjectName(_fromUtf8("groupBox_X"))
-        
-
-            
-            
+        self.groupBox_Type = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox_Type.setGeometry(QtCore.QRect(730, 80, 101, 191))
+        self.groupBox_Type.setTitle(QtGui.QApplication.translate("RecordSweepWindow", "Type", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox_Type.setObjectName(_fromUtf8("groupBox_Type"))
+        self.groupBox_Name = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox_Name.setGeometry(QtCore.QRect(650, 80, 81, 191))
+        self.groupBox_Name.setTitle(QtGui.QApplication.translate("RecordSweepWindow", "Channel Name", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox_Name.setObjectName(_fromUtf8("groupBox_Name"))
+        self.groupBox_Instr = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox_Instr.setGeometry(QtCore.QRect(830, 80, 81, 191))
+        self.groupBox_Instr.setTitle(QtGui.QApplication.translate("RecordSweepWindow", "Instrument", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox_Instr.setObjectName(_fromUtf8("groupBox_Instr"))
         RecordSweepWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(RecordSweepWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 966, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1035, 21))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         RecordSweepWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(RecordSweepWindow)
