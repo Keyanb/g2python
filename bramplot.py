7475d69a72334192b8ac17de5679401fc8a82f54
Bram Evert <bram.evert@gmail.com>
2013-02-28 10:41:24 -0500

    UpdateInterface

--- bramplot.py
+++ bramplot.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'bramplot.ui'
 #
-# Created: Thu Feb 21 16:24:29 2013
+# Created: Wed Feb 27 16:41:54 2013
 #      by: PyQt4 UI code generator 4.8.6
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,16 +17,29 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(790, 398)
+        MainWindow.resize(976, 600)
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.mplwidget = MatplotlibWidget(self.centralwidget)
-        self.mplwidget.setGeometry(QtCore.QRect(10, 10, 761, 331))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(830, 20, 101, 41))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "Controls to go here", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.widget = QtGui.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(10, 10, 763, 599))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout.setMargin(0)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.mplwidget = MatplotlibWidget(self.widget)
         self.mplwidget.setObjectName(_fromUtf8("mplwidget"))
+        self.verticalLayout.addWidget(self.mplwidget)
+        self.mplwidget_2 = MatplotlibWidget(self.widget)
+        self.mplwidget_2.setObjectName(_fromUtf8("mplwidget_2"))
+        self.verticalLayout.addWidget(self.mplwidget_2)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 790, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 976, 21))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
